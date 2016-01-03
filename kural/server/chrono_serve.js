e7b1018091b8e5e2a2a926749b7911b16fb51ae8
Suriyadeepan Ram <suriyadeepan@acm.org>
2016-01-03 23:54:52 +0530

    recognize request message as string

--- kural/server/chrono_serve.js
+++ kural/server/chrono_serve.js
@@ -1,10 +1,17 @@
 var http = require("http");
+var body = []
 
 http.createServer(function(request, response) {
 
 	request.on('data',function(chunk){
-		console.log(chunk)
+		//console.log(chunk)
+		body.push(chunk)
 	}).on('end',function(){
+		// print the accumulated content
+		console.log(Buffer.concat(body).toString());
+		//console.log(body)
+
+		// respond
 	  response.writeHead(200, {"Content-Type": "text/plain"});
  		response.write("<meta>nil</meta>");
 		response.write("<msg>I have received your request</msg>");
